948e811ec9c70ce33d6a1d5684c8d2aeda766276
k-vkarna <kkepchukk@yandex.ru>
2022-06-22 16:07:12 +0700

    upload

---
+++ Examples for SANDM/hello.asm
@@ -0,0 +1,21 @@
+set		d1819043144			;00
+save	d100				;01
+set		d 1867980911		;02
+save	d 104				;03
+set		d 560229490			;04
+save	d 108				;05
+set		w 3338				;06
+save	d 112				;07
+;comment
+set		b 99				;08
+save	b 0					;09
+set		& b 0				;10
+add		b1					;11
+save	b0					;12
+load	b					;13
+jnz		17					;14
+set		1					;15
+jnz		19					;16
+write	c					;17
+jnz		10					;18
+nop							;19
